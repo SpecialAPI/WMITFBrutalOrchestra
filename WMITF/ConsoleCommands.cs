ce1d84d28f99819c5c3c715d4edd580cb8b3b972
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2025-09-04 01:17:32 +0300

    Add "wmitf character" command

--- WMITF/ConsoleCommands.cs
+++ WMITF/ConsoleCommands.cs
@@ -36,6 +36,30 @@ namespace WMITF
                 else
                     DebugController.Instance.WriteLine($"{itm.GetItemLocData().text} ({itm.name}) is either not modded or is not recognized by WMITF.");
             }));
+
+            group.children.Add(new DebugCommand("character", "Tells you what mod a character is from.", new()
+            {
+                new StringCommandArgument("characterID", DebugController.CharacterAutocomplete)
+            }, x =>
+            {
+                var id = x[0].Read<string>();
+
+                if (id == null)
+                    return;
+
+                var ch = LoadedAssetsHandler.GetCharacter(id);
+
+                if (ch == null)
+                {
+                    DebugController.Instance.WriteLine($"Unknown character \"{id}\".", LogLevel.Error);
+                    return;
+                }
+
+                if (PluginFinder.ModdedCharacterPlugins.TryGetValue(ch.name, out var plugin))
+                    DebugController.Instance.WriteLine($"{ch.GetName()} ({ch.name}) is from {plugin.Metadata.Name ?? "[null plugin name, this should not be happening]"} ({plugin.Metadata.GUID ?? "[null plugin GUID, this should not be happening"})");
+                else
+                    DebugController.Instance.WriteLine($"{ch.GetName()} ({ch.name}) is either not modded or is not recognized by WMITF.");
+            }));
         }
     }
 }
