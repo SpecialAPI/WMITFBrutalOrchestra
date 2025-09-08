bbcd6825a2f03d1b262c7e30b83944c79c3142f4
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2025-09-08 21:46:16 +0300

    Add glossary displays for status and field effects

--- WMITF/ModDisplayFieldEffects.cs
+++ WMITF/ModDisplayFieldEffects.cs
@@ -14,7 +14,8 @@ namespace WMITF
         public static MethodInfo listSlotStatusEffectInfoSOGetItem = AccessTools.Method(typeof(List<SlotStatusEffectInfoSO>), "get_Item");
 
         public static MethodInfo dm_cet_ssei = AccessTools.Method(typeof(ModDisplayFieldEffects), nameof(DisplayMod_CharacterEnemyTooltip_SaveStatusEffectInfo));
-        public static MethodInfo dm_amn = AccessTools.Method(typeof(ModDisplayFieldEffects), nameof(DisplayMod_AddModName));
+        public static MethodInfo dm_c_amn = AccessTools.Method(typeof(ModDisplayFieldEffects), nameof(DisplayMod_Combat_AddModName));
+        public static MethodInfo dm_g_amn = AccessTools.Method(typeof(ModDisplayFieldEffects), nameof(DisplayMod_Glossary_AddModName));
 
         [HarmonyPatch(typeof(CombatVisualizationController), nameof(CombatVisualizationController.ShowcaseCharacterFieldTooltip))]
         [HarmonyPatch(typeof(CombatVisualizationController), nameof(CombatVisualizationController.ShowcaseEnemyFieldTooltip))]
@@ -35,10 +36,37 @@ namespace WMITF
                 return;
 
             crs.Emit(OpCodes.Ldloc, slotStatusEffectInfoLoc);
-            crs.Emit(OpCodes.Call, dm_amn);
+            crs.Emit(OpCodes.Call, dm_c_amn);
         }
 
-        public static string DisplayMod_AddModName(string orig, SlotStatusEffectInfoSO info)
+        [HarmonyPatch(typeof(ExtraInformationUIHandler), nameof(ExtraInformationUIHandler.SetGlossaryInformation), typeof(SlotStatusEffectInfoSO))]
+        [HarmonyILManipulator]
+        public static void DisplayMod_Glossary_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchLdfld<StringPairData>(nameof(StringPairData.description))))
+                return;
+
+            crs.Emit(OpCodes.Ldarg_1);
+            crs.Emit(OpCodes.Call, dm_g_amn);
+        }
+
+        public static string DisplayMod_Glossary_AddModName(string orig, SlotStatusEffectInfoSO info)
+        {
+            if (!(ModConfig.ShowModsForFieldEffects is StatusFieldDisplayCondition.On or StatusFieldDisplayCondition.OnlyInGlossary))
+                return orig;
+
+            if (orig == null)
+                return orig;
+
+            if (!PluginFinder.TryGetFieldEffectModName(info, out var modName))
+                return orig;
+
+            return $"{orig}\n\n{modName}";
+        }
+
+        public static string DisplayMod_Combat_AddModName(string orig, SlotStatusEffectInfoSO info)
         {
             if (ModConfig.ShowModsForFieldEffects != StatusFieldDisplayCondition.On)
                 return orig;
--- WMITF/ModDisplayStatusEffects.cs
+++ WMITF/ModDisplayStatusEffects.cs
@@ -14,7 +14,8 @@ namespace WMITF
         public static MethodInfo listStatusEffectInfoSOGetItem = AccessTools.Method(typeof(List<StatusEffectInfoSO>), "get_Item");
 
         public static MethodInfo dm_cet_ssei = AccessTools.Method(typeof(ModDisplayStatusEffects), nameof(DisplayMod_CharacterEnemyTooltip_SaveStatusEffectInfo));
-        public static MethodInfo dm_amn = AccessTools.Method(typeof(ModDisplayStatusEffects), nameof(DisplayMod_AddModName));
+        public static MethodInfo dm_c_amn = AccessTools.Method(typeof(ModDisplayStatusEffects), nameof(DisplayMod_Combat_AddModName));
+        public static MethodInfo dm_g_amn = AccessTools.Method(typeof(ModDisplayStatusEffects), nameof(DisplayMod_Glossary_AddModName));
 
         [HarmonyPatch(typeof(CombatVisualizationController), nameof(CombatVisualizationController.ShowcaseCharacterStatusTooltip))]
         [HarmonyPatch(typeof(CombatVisualizationController), nameof(CombatVisualizationController.ShowcaseEnemyStatusTooltip))]
@@ -35,7 +36,7 @@ namespace WMITF
                 return;
 
             crs.Emit(OpCodes.Ldloc, statusEffectInfoLoc);
-            crs.Emit(OpCodes.Call, dm_amn);
+            crs.Emit(OpCodes.Call, dm_c_amn);
         }
 
         [HarmonyPatch(typeof(CombatVisualizationController), nameof(CombatVisualizationController.ShowcaseInfoStatusTooltip))]
@@ -48,10 +49,37 @@ namespace WMITF
                 return;
 
             crs.Emit(OpCodes.Ldloc_0);
-            crs.Emit(OpCodes.Call, dm_amn);
+            crs.Emit(OpCodes.Call, dm_c_amn);
         }
 
-        public static string DisplayMod_AddModName(string orig, StatusEffectInfoSO info)
+        [HarmonyPatch(typeof(ExtraInformationUIHandler), nameof(ExtraInformationUIHandler.SetGlossaryInformation), typeof(StatusEffectInfoSO))]
+        [HarmonyILManipulator]
+        public static void DisplayMod_Glossary_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchLdfld<StringPairData>(nameof(StringPairData.description))))
+                return;
+
+            crs.Emit(OpCodes.Ldarg_1);
+            crs.Emit(OpCodes.Call, dm_g_amn);
+        }
+
+        public static string DisplayMod_Glossary_AddModName(string orig, StatusEffectInfoSO info)
+        {
+            if (!(ModConfig.ShowModsForStatusEffects is StatusFieldDisplayCondition.On or StatusFieldDisplayCondition.OnlyInGlossary))
+                return orig;
+
+            if (orig == null)
+                return orig;
+
+            if (!PluginFinder.TryGetStatusEffectModName(info, out var modName))
+                return orig;
+
+            return $"{orig}\n\n{modName}";
+        }
+
+        public static string DisplayMod_Combat_AddModName(string orig, StatusEffectInfoSO info)
         {
             if (ModConfig.ShowModsForStatusEffects != StatusFieldDisplayCondition.On)
                 return orig;
